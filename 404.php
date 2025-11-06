06caf7479f14de2b539b3a852280f9ac6e07435a
BOLLUNIKHIL <bollunikhil21@gmail.com>
2025-11-06 14:09:25 +0100

    add page

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
             <h1 class="display-1 text-muted">404</h1>
             <h2 class="mb-4">Page Not Found</h2>
             <p class="lead mb-4">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
-            <a href="/" class="btn btn-primary">Return to Home</a>
+            <a href="/" class="btn btn-primary">Return to Home Page</a>
         </div>
     </div>
 </div>
