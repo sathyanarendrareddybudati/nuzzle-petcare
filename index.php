596f6ea90446ac1681e42dd4667b887c3a4eb972
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2025-12-17 01:12:10 +0100

    session ini

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 declare(strict_types=1);
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/bootstrap/app.php';
+
+require_once __DIR__ . '/src/Core/helpers.php';
+
+if (class_exists(\Dotenv\Dotenv::class) && is_file(__DIR__ . '/.env')) {
+    \Dotenv\Dotenv::createImmutable(__DIR__)->load();
+}
 
 use App\Core\Router;
 
 $router = new Router();
 
-// Load routes
 require __DIR__ . '/routes/web.php';
 
-// Dispatch
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '/';
 $router->dispatch($_SERVER['REQUEST_METHOD'], rtrim($path, '/') ?: '/');
