3da419557ea238759117a01db511500e556192ee
BurelliTarun <tarunburelli@gmail.com>
2026-01-14 14:59:49 +0100

    terms of service view updated

---
+++ views/terms-of-service/index.php
@@ -0,0 +1,56 @@
+<div class="container my-5">
+    <h1 class="display-5 fw-bold mb-4">Terms of Service</h1>
+
+    <p>Welcome to PetCare! These terms and conditions outline the rules and regulations for the use of PetCare's Website, located at petcare.com.</p>
+
+    <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use PetCare if you do not agree to take all of the terms and conditions stated on this page.</p>
+
+    <h2 class="h4 mt-4">Cookies</h2>
+    <p>We employ the use of cookies. By accessing PetCare, you agreed to use cookies in agreement with the PetCare's Privacy Policy.</p>
+
+    <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+
+    <h2 class="h4 mt-4">License</h2>
+    <p>Unless otherwise stated, PetCare and/or its licensors own the intellectual property rights for all material on PetCare. All intellectual property rights are reserved. You may access this from PetCare for your own personal use subjected to restrictions set in these terms and conditions.</p>
+
+    <p>You must not:</p>
+    <ul>
+        <li>Republish material from PetCare</li>
+        <li>Sell, rent or sub-license material from PetCare</li>
+        <li>Reproduce, duplicate or copy material from PetCare</li>
+        <li>Redistribute content from PetCare</li>
+    </ul>
+
+    <p>This Agreement shall begin on the date hereof.</p>
+
+    <h2 class="h4 mt-4">User Comments</h2>
+    <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. PetCare does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of PetCare,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, PetCare shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+
+    <p>PetCare reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+
+    <p>You warrant and represent that:</p>
+    <ul>
+        <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+        <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+        <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+        <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+    </ul>
+
+    <p>You hereby grant PetCare a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+
+    <h2 class="h4 mt-4">Disclaimer</h2>
+    <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+
+    <ul>
+        <li>limit or exclude our or your liability for death or personal injury;</li>
+        <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+        <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+        <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+    </ul>
+
+    <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
+
+    <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+
+    <p class="mt-5 text-muted"><em>This is a sample Terms of Service. You should replace this with your own.</em></p>
+</div>
